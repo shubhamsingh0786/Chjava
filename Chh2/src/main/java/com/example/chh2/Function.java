41badd65a313ca4ffd37d80b7223a05b5e4d78e0
shhubhamsingh0786 <shubhamsingh49886@gmail.com>
2021-08-17 03:07:03 +0530

    Initial commit

---
+++ Chh2/src/main/java/com/example/chh2/Function.java
@@ -0,0 +1,13 @@
+package com.example.chh2;
+
+public class Function {
+
+    public static void main(String[] args) {
+         int a=65;
+        int b=5;
+        int c=78;
+        double avg =(a+b+c)/3.0;
+        System.out.println(avg);
+
+    }
+}
---
+++ Chh2/src/main/java/com/example/chh2/MyClass.java
@@ -0,0 +1,38 @@
+package com.example.chh2;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Android");
+        System.out.println("Hello Shubham");
+        //this is a String
+        String name = "This is my name";
+        System.out.println(name.substring(2));
+        int a = 25;
+        int b = 34;
+        /*
+         for print the output*/
+        System.out.println(name);
+
+        System.out.println(a-b);
+//        if-else conditonals
+        int var =9;
+        if (var>45){
+
+
+            System.out.println("the value is greater than 45");
+        }
+        else{
+            if(var==8){
+                System.out.println("wow this is an eight");
+
+            }
+            System.out.println("the value is laser than 45");
+            }
+// loops in java
+        for(int i=0;i<8;i++)
+            System.out.println(i);
+
+        }
+
+
+}
