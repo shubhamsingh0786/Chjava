756bb55aa0b8882f5285696f724202f4ce6c6e09
shhubhamsingh0786 <shubhamsingh49886@gmail.com>
2021-08-17 14:50:04 +0530

    Commit

---
+++ Chh2/src/main/java/com/example/chh2/Array.java
@@ -0,0 +1,21 @@
+package com.example.chh2;
+
+import java.util.ArrayList;
+
+public class Array {
+    public static void main(String[] args) {
+        int Shubham[]={2,4,5,7,3,40};
+        System.out.println(Shubham[3]);
+        System.out.println(Shubham[0]);
+        System.out.println(Shubham[4]);
+        ArrayList p = new ArrayList();
+        p.add(3);
+        p.add("4352");
+        p.add(49);
+        for (Object o:p){
+            System.out.println("object is:"+o);
+
+        }
+
+    }
+}
